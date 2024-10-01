cc0b32cc3dba4377974a2813852c82cd08ca7356
kassap11 <154228160+kassap11@users.noreply.github.com>
2024-10-01 22:37:01 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+img = document.querySelector(".img");
+container = document.querySelector(".container");
+function phones(phone) {
+  img.src = phone;
+}
+function colors(color) {
+  container.style.background = color;
+}
